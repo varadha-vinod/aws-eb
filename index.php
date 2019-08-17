553918ee55e78fd0f74e78bf8d14385abc921f1a
codepipe <47555726+codepipe@users.noreply.github.com>
2019-08-17 13:11:44 +0530

    Update index.php

--- index.php
+++ index.php
@@ -15,6 +15,7 @@ if(!isset($_COOKIE[$cookie_name])) {
      echo "Value is: " . $_COOKIE[$cookie_name];
 }
 ?>
+     https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fen%2Fthumb%2Fd%2Fdc%2FBaahubali_The_Beginning_Movie_Poster.jpg%2F220px-Baahubali_The_Beginning_Movie_Poster.jpg&imgrefurl=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FBaahubali%3A_The_Beginning&docid=l8qRlRN2dElgYM&tbnid=M-pxE-7wK-iG4M%3A&vet=10ahUKEwjOtczUs4nkAhVLK48KHfiKBpMQMwh3KAIwAg..i&w=220&h=326&bih=608&biw=1024&q=bahubali&ved=0ahUKEwjOtczUs4nkAhVLK48KHfiKBpMQMwh3KAIwAg&iact=mrc&uact=8
 
 <p><strong>Note:</strong> You might have to reload the page to see the value of the cookie.</p>
 
